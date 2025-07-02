0702f1383fedf6442a4d3a8d1361daa222b110de
NGO <ngoyarzo@gmail.com>
2025-07-02 02:58:54 -0400

    cors

--- apps/nodejs-prisma/src/middlewares/cors.js
+++ apps/nodejs-prisma/src/middlewares/cors.js
@@ -1,21 +1,17 @@
 import cors from 'cors';
 
-const ACCEPTED_ORIGINS = process.env.ACCEPTED_ORIGINS
-  ? process.env.ACCEPTED_ORIGINS.split(',')
-  : [];
-
-const NODE_ENV = process.env.NODE_ENV || 'development';
-const isDevelopment = NODE_ENV === 'development';
+const ACCEPTED_ORIGINS = process.env.ACCEPTED_ORIGINS === '*'
+  ? '*'
+  : process.env.ACCEPTED_ORIGINS?.split(',') || [];
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) =>
   cors({
     origin: (origin, callback) => {
-      // Permitir sin origin solo en desarrollo
       if (!origin && isDevelopment) {
         return callback(null, true);
       }
 
-      if (acceptedOrigins.includes(origin)) {
+      if (acceptedOrigins === '*' || acceptedOrigins.includes(origin)) {
         return callback(null, true);
       }
 
