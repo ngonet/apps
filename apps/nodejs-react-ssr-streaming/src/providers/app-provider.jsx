e41e70eb109c59f3fad5075107b4a690b35c4f25
NGO <ngoyarzo@gmail.com>
2025-07-07 00:33:09 -0400

    new image ssr

--- apps/nodejs-react-ssr-streaming/src/providers/app-provider.jsx
+++ apps/nodejs-react-ssr-streaming/src/providers/app-provider.jsx
@@ -4,7 +4,7 @@ import 'react-notifications-component/dist/theme.css';
 import { AppContext } from './app-context';
 
 export const AppProvider = ({ children }) => {
-  const [companyId, setCompanyId] = useState(import.meta.env.VITE_COMPANY_ID);
+  const [companyId, setCompanyId] = useState(process.env.COMPANY_ID);
 
   const value = useMemo(
     () => ({
